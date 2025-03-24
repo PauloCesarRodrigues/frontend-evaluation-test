5dc7f564420147305ad4118bc292bcfe1ea655b5
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-03-24 10:26:15 -0300

    feature: Login form validation

--- src/components/loginComponents.tsx
+++ src/components/loginComponents.tsx
@@ -1,18 +1,48 @@
+import { loginSchema, type loginType } from "@/schemas/schemaLogin";
+import type { registerForm } from "@/schemas/schemaRegister";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 export function Login() {
+
+  const {register, formState:{errors, isLoading}, handleSubmit} = useForm<registerForm>({
+    resolver: zodResolver(loginSchema)
+  })
+
+  function handleLogin(LoginData: loginType){
+    /*const response = fetch('',{
+    })*/
+
+    console.log(LoginData)
+  }
+
+
   return (
     <div>
       <h1>Login</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(handleLogin)}>
         <div>
+          <label>name: </label>
+          <input type="text" placeholder="Digite seu nome" {...register('name')}/>
+          {errors.name &&(
+            <p>{errors.name.message}</p>
+          )}
+
           <label>Email: </label>
-          <input type="email" placeholder="Digite seu email" />
+          <input type="email" placeholder="Digite seu email" {...register('email')}/>
+          {errors.email &&(
+            <p>{errors.email.message}</p>
+          )}
 
           <label>Senha:</label>
-          <input type="password" placeholder="Digite sua senha" />
+          <input type="password" placeholder="Digite sua senha" {...register('password')}/>
+          {errors.password &&(
+            <p>{errors.password.message}</p>
+          )}
         </div>
         <div>
-          <button type="submit">Entrar</button>
+          <button type="submit" disabled={isLoading}>Entrar</button>
         </div>
       </form>
     </div>
